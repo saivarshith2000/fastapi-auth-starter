425d97de335f84fb2ce8d667eebfb0c0af4898e9
Sai Varshith, Hariyala <hosvarshith@gmail.com>
2023-08-07 21:32:32 +0530

    updated configs to use DATABASE_URL environment variable

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -11,6 +12,13 @@ from db.models import user
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Inject DATABASE_URL env var in alembic config
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("Enviornment variable 'DATABASE_URL' is required")
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/db/setup.py
+++ app/db/setup.py
@@ -1,7 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_engine(url="postgresql+psycopg2://localhost:localhost@localhost/auth")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(url=DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
